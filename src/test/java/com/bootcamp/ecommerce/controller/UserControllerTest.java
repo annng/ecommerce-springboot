d949e21dbe4c0b00a9c1f43a687e7b9b4b077b80
annng <anangpambudi101@gmail.com>
2023-08-23 16:49:31 +0700

    add testing controller for add / find data user

--- src/test/java/com/bootcamp/ecommerce/controller/UserControllerTest.java
+++ src/test/java/com/bootcamp/ecommerce/controller/UserControllerTest.java
@@ -5,6 +5,7 @@ import com.bootcamp.ecommerce.entity.User;
 import com.bootcamp.ecommerce.model.UserDto;
 import com.bootcamp.ecommerce.repository.UserRepository;
 import com.bootcamp.ecommerce.service.UserService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -15,41 +16,50 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
+@RequestMapping
 public class UserControllerTest {
 
-    @MockBean
+    @Autowired
     UserService userService;
 
-    @Autowired
+    @MockBean
     UserRepository userRepository;
 
-    @Autowired
     private MockMvc mockMvc;
-
-    @Test
-    void getUserTest() throws Exception{
-
-    }
 
     @Test
     @Rollback(value = false)
     @Order(1)
     void addUser(){
         User saved = User.builder()
-                .id(3L)
+                .id(1L)
                 .name("Pengguna")
                 .address("Semarang")
                 .build();
 
-        when(userRepository.save(Mockito.mock(User.class))).thenReturn(saved);
+        when(userRepository.save(any(User.class))).thenReturn(saved);
 
-        ResponseEntity<UserDto> resp = userService.postUser(UserDto.builder().build());
-        assertEquals(resp, UserDto.builder().build());
+        ResponseEntity<UserDto> resp = userService.postUser(UserDto.builder()
+                .name("Pengguna")
+                .address("Semarang")
+                .build());
+
+        ResponseEntity<UserDto> expected = new ResponseEntity<>(UserDto.builder()
+                .id(1L)
+                .name("Pengguna")
+                .address("Semarang").build(), HttpStatus.OK);
+
+        Assertions.assertEquals(expected, resp);
     }
 
     @Test
@@ -57,14 +67,19 @@ public class UserControllerTest {
     @Order(2)
     void getUserById(){
         User saved = User.builder()
-                .id(3L)
+                .id(1L)
                 .name("Pengguna")
                 .address("Semarang")
                 .build();
 
-        when(userRepository.save(Mockito.mock(User.class))).thenReturn(saved);
+        when(userRepository.findById(1L)).thenReturn(Optional.of(saved));
 
-        ResponseEntity<UserDto> resp = userService.getUser(3L);
-        assertEquals(resp, new ResponseEntity<>(UserDto.builder().build(), HttpStatus.OK));
+        ResponseEntity<UserDto> resp = userService.getUser(1L);
+        ResponseEntity<UserDto> expected = new ResponseEntity<>(UserDto.builder()
+                .id(1L)
+                .name("Pengguna")
+                .address("Semarang")
+                .build(), HttpStatus.OK);
+        Assertions.assertEquals(resp, expected);
     }
 }
